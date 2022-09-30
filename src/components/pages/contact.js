25f62f8106e9cff65a50617ae14d7764d06bad39
Josh Souter-Brown <josh.gravicast@gmail.com>
2022-09-30 15:18:17 -0400

    restyled the splash pages

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -21,82 +21,84 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <div className="examples">
-                <div className="example_header">
-                    <h2>What People Love to Log</h2>
+            <div className="footer">
+                <div className="examples">
+                    <div className="example_header">
+                        <h2>What People Love to Log</h2>
+                    </div>
                 </div>
-            </div>
 
-            <div className="catagories">
-                <div className="catagory">
-                    <h4>Activities</h4>
+                <div className="catagories">
+                    <div className="catagory">
+                        <h4>Activities</h4>
+                    </div>
+                    <div className="catagory">
+                        <h4>Things</h4>
+                    </div>
+                    <div className="catagory">
+                        <h4>Places</h4>
+                    </div>
                 </div>
-                <div className="catagory">
-                    <h4>Things</h4>
-                </div>
-                <div className="catagory">
-                    <h4>Places</h4>
-                </div>
-            </div>
 
-            <div className="items_container">
-                <div className="items">
-                    <div className="item">
-                        <h4>Riding</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Dancing</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Eating</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Watching</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Driving</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Cooking</h4>
-                    </div>
+                <div className="items_container">
+                    <div className="items">
+                        <div className="item">
+                            <h4>Riding</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Dancing</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Eating</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Watching</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Driving</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Cooking</h4>
+                        </div>
 
-                </div>
-                <div className="items">
-                    <div className="item">
-                        <h4>Rollercoasters</h4>
                     </div>
-                    <div className="item">
-                        <h4>Dances</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Food</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Movies</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Vehicals</h4>
-                    </div>
+                    <div className="items">
+                        <div className="item">
+                            <h4>Rollercoasters</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Dances</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Food</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Movies</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Vehicals</h4>
+                        </div>
 
-                </div>
-                <div className="items">
-                    <div className="item">
-                        <h4>Theme Parks</h4>
                     </div>
-                    <div className="item">
-                        <h4>Clubs</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Theaters</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Restaurants</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Outside</h4>
-                    </div>
-                    <div className="item">
-                        <h4>Museums</h4>
+                    <div className="items">
+                        <div className="item">
+                            <h4>Theme Parks</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Clubs</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Theaters</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Restaurants</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Outside</h4>
+                        </div>
+                        <div className="item">
+                            <h4>Museums</h4>
+                        </div>
                     </div>
                 </div>
             </div>
